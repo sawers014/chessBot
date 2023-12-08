eb647023cc3e64a19cc1f45f1baf7ce7dc4fea84
sawers014 <74411308+sawers014@users.noreply.github.com>
2023-12-08 21:38:23 +0100

    Update printBoard.py
    now it works

--- printBoard.py
+++ printBoard.py
@@ -12,35 +12,37 @@
 -9 represent a black queen
 10 represent THE white king
 -10 represent THE black king
-
 """
-           #A  B  C  D  E  F  G  H
-chessboard=[0, 0, 0, 0, 0, 0, 0, 0, #8
-            0, 0, 0, 0, 0, 0, 0, 0, #7
-            0, 0, 0, 0, 0, 0, 0, 0, #6
-            0, 0, 0, 0, 0, 0, 0, 0, #5
-            0, 0, 0, 0, 0, 0, 0, 0, #4
-            0, 0, 0, 0, 0, 0, 0, 0, #3
-            0, 0, 0, 0, 0, 0, 0, 0, #2
-            0, 0, 0, 0, 0, 0, 0, 0, #1
-            ]
-for x in range(8):
-    print("\n")
-    for j in range(8):
-                
-                switch={
-                0:'X',
-                1:'♟︎',
-                -1:'♙',
-                3:'♝',
-                -3:'♗',
-                2.9:'♞',
-                -2.9:'♘',
-                5:'♜',
-                -5:'♖',
-                9:'♛',
-                -9:'♕',
-                10:'♚',
-                -10:'♔'
-                }
-                print("|", switch.get(3,"Invalid input")," ", end="") #print the piece with a subdivision
+def printBoard(board): #this function prints a chessboard given as input
+    index=0
+    for x in range(8):
+        print("\n")
+        for j in range(8):
+                    
+                    switch={
+                    0:'X',
+                    1:'♟︎',
+                    -1:'♙',
+                    3:'♝',
+                    -3:'♗',
+                    2.9:'♞',
+                    -2.9:'♘',
+                    5:'♜',
+                    -5:'♖',
+                    9:'♛',
+                    -9:'♕',
+                    10:'♚',
+                    -10:'♔'
+                    }
+                    print("|", switch.get(board[index],"Invalid input")," ", end="") #print the piece with a subdivision
+                    index+=1    #increase the index 
+              #A   B   C  D  E   F  G    H
+initialBoard=[-5,-2.9,-3,-9,-10,-3,-2.9,-5, #8
+              -1,-1,  -1,-1, -1,-1,-1,  -1, #7
+               0, 0,   0, 0, 0,  0, 0,   0, #6
+               0, 0,   0, 0, 0,  0, 0,   0, #5
+               0, 0,   0, 0, 0,  0, 0,   0, #4
+               0, 0,   0, 0, 0,  0, 0,   0, #3
+               1, 1,   1, 1, 1,  1, 1,   1, #2
+               5, 2.9, 3, 9, 10, 3, 2.9, 5  #1
+              ]
