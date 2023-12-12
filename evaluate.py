7218782caa4776679a83b18bd945681ec54a0272
sawers014 <74411308+sawers014@users.noreply.github.com>
2023-12-12 22:06:43 +0100

    Update evaluate.py
    now it will think that putting a knight in the center is better

--- evaluate.py
+++ evaluate.py
@@ -13,9 +13,36 @@
 10 represent THE white king
 -10 represent THE black king
 """
+"""
+TODO:
+-use colored ASCII;
+-function to see if the king is on check;
+-ability to create treats;
+-function to see checkmate in 1;
+-ability to try and avoid pins;
+-not fixed values based on the position(a knight in the center should be valued more than a knight in the corner);
+-actually evaluate.
+"""
+notation = {
+    0: "a8", 1:"b8", 2:"c8", 3:"d8", 4:"e8", 5:"f8", 6:"g8", 7:"h8",
+    8: "a7",9: "b7",10:"c7",11:"d7",12:"e7",13:"f7",14:"g7",15:"h7",
+    16:"a6",17:"b6",18:"c6",19:"d6",20:"e6",21:"f6",22:"g6",23:"h6",
+    24:"a5",25:"b5",26:"c5",27:"d5",28:"e5",29:"f5",30:"g5",31:"h5",
+    32:"a4",33:"b4",34:"c4",35:"d4",36:"e4",37:"f4",38:"g4",39:"h4",
+    40:"a3",41:"b3",42:"c3",43:"d3",44:"e3",45:"f3",46:"g3",47:"h3",
+    48:"a2",49:"b2",50:"c2",51:"d2",52:"e2",53:"f2",54:"g2",55:"h2",
+    56:"a1",57:"b1",58:"c1",59:"d1",60:"e1",61:"f1",62:"g1",63:"h1"   
+}
 def evaluate(board):
     x=0
-    for y in board:
+    for z in range(len(board)):#its useful to cycle by index
+        y=board[z]  #its better to do, so we can understand where the piece is.
+        if y==2.9 or y==-2.9:
+            position=notation[z] #localize Where a piece is
+            if position[0]=="c"  or position[0]=="d" or position[0]=="e" or position[0]=="f":
+                y*=1.1 #a knight should be valued more because it is in a central position
+                       #we use the multiplication because we dont know if the knight is white or black
+
         x=round(x, 1) + y   #need to do this because we cant work with int because it is easier to evaluate some piece with 
                             #decimals based on their position on the board
         print("for now x is equal to ", x)
@@ -31,3 +58,5 @@ initialBoard=[-5,-2.9,-3,-9,-10,-3,-2.9,-5, #8
                1, 1,   1, 1, 1,  1, 1,   1, #2
                5, 2.9, 3, 9, 10, 3, 2.9, 5  #1
               ]
+
+print("should return a -1, but we get ", evaluate(initialBoard))
